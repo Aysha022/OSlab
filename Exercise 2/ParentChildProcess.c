21faebb1398a1c20b2e5a5c1b9205862ffed134d
Aysha Naurin <149690596+Aysha022@users.noreply.github.com>
2024-03-24 15:48:46 +0530

    Update ParentChildProcess.c

--- Exercise 2/ParentChildProcess.c
+++ Exercise 2/ParentChildProcess.c
@@ -1,7 +1,28 @@
-#inlcude<stdio.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<stdlib.h>
+
 void main()
 {
-  int pid;
-  pid=fork();
-  
+    int pro;
+    pro=fork();
+
+    if(pro < 0)
+    {
+        printf("error");
+    }
+    else if(pro == 0)
+    {
+        printf("parant process ID is %d and child process ID is %d",getppid(),getpid());
+        printf("child process terminated !!!");
+        exit(0);
+    }
+    else
+    {
+        wait(NULL);
+        printf("parent process terminated !!!");
+        exit(2);
+    }
 }
