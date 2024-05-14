cf57dd6d2db47c3f5b1841641161fc57fc1cf2b3
Aysha Naurin <149690596+Aysha022@users.noreply.github.com>
2024-05-14 18:32:41 +0530

    Update ChatServer_SharedMemory.c

--- Exercise 5/ChatServer_SharedMemory.c
+++ Exercise 5/ChatServer_SharedMemory.c
@@ -1,1 +1,15 @@
+Sender.c
+--------
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/mman.h>
+#include<sys/stat.h>
+#include<string.h>
+#include<stdlib.h>
 
+typedef struct 
+{
+  char msg[100];
+  int c,exit;
+}resource;
